4a88b033ff5b8c7e5e3c7bcb0caa0772d76a5254
MichelleNguy <plasticwatering@gmail.com>
2019-10-30 22:43:16 -0400

    Added adapter

---
+++ src/adapter.js
@@ -0,0 +1,25 @@
+const API_URL = 'http://localhost:3000';
+
+const HEADERS = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+};
+
+// Handle all POST requests to backend API
+// example: http://localhost:3000/users
+const fetchPost = (objectToPost, slug) => {
+    return fetch(`${API_URL}/${slug}`, {
+        method: 'POST',
+        HEADERS,
+        body: JSON.stringify(objectToPost)
+    }).then(res => res.json())
+}
+
+// Handle all GET requests to backend API
+// example: http://localhost:3000/users/1
+const fetchGet = (slug) => {
+    return fetch(`${API_URL}/${slug}`)
+        .then(res => res.json());
+}
+
+export { API_URL, HEADERS, fetchPost, fetchGet}
