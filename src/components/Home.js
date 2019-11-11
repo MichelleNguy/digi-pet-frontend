7bdf7fb54fc04f80b31723dc9b5ce1c3f08f43fa
MichelleNguy <plasticwatering@gmail.com>
2019-11-10 22:42:03 -0500

    css changes and added content to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,19 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            This is the home page with a bunch of information on the game/how to play the game
+            <h1>Welcome to Digi-Pets!</h1>
+            <div id="home-container">
+                <p>
+                    Digi-pets is a casual game where you can make pets, 
+                    and go shopping for items to care for your pet. 
+                </p>
+                <p>
+                    Logging in every day earns you money in which you can
+                    use to buy items for your pet.
+                </p>
+            </div>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </div>
     )
 }
